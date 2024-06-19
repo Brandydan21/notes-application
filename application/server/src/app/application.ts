85156eed4daea2b8840f2deb509d652e4541487d
Brandydan21 <brandydan2002@gmail.com>
2024-06-19 22:27:26 +1000

    Initial commit

---
+++ application/server/src/app/application.ts
@@ -0,0 +1,13 @@
+import express, {Express} from 'express';
+
+export class Application{
+    private _server: Express;
+    constructor(){
+        this._server = express();
+        
+    }
+
+
+
+
+}
